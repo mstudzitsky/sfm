5991013dae64ab60e6da9e36f7293f69bff5cf70
Michael Studzitsky <mstudzitsky@gmail.com>
2022-03-29 18:05:03 +0600

    add manager

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mstudzitsky/sfm
+
+go 1.18
+
+require (
+	github.com/xbsoftware/wfs v0.0.0-20200826093531-5710d5a7e63b
+	github.com/xbsoftware/wfs-local v0.0.0-20211112093318-a0c3a7cc5558
+)
---
+++ manager.go
@@ -0,0 +1,52 @@
+package sfm
+
+import (
+	"github.com/xbsoftware/wfs"
+	"strings"
+)
+import "github.com/xbsoftware/wfs-local"
+
+type SiteFileManager struct {
+	RootPath    string
+	UploadLimit uint
+	Drive       wfs.Drive
+}
+
+func NewSiteFileManager(RootPath string, UploadLimit uint) (*SiteFileManager, error) {
+	policy := wfs.Policy(&wfs.AllowPolicy{})
+	driveConfig := &wfs.DriveConfig{
+		Verbose: true,
+		Policy:  &policy,
+	}
+	drive, err := local.NewLocalDrive(RootPath, driveConfig)
+	if err != nil {
+		return nil, err
+	}
+	return &SiteFileManager{
+		RootPath:    RootPath,
+		UploadLimit: UploadLimit,
+		Drive:       drive,
+	}, nil
+}
+
+func (s *SiteFileManager) ls(dir string, ext ...string) ([]wfs.File, error) {
+	config := wfs.ListConfig{
+		SkipFiles:  false,
+		SubFolders: true,
+		Nested:     true,
+		Exclude:    func(name string) bool { return strings.HasPrefix(name, ".") },
+		Include: func(name string) bool {
+			for _, e := range ext {
+				if strings.HasSuffix(name, e) {
+					return true
+				}
+			}
+			return false
+		},
+	}
+	files, err := s.Drive.List(dir, &config)
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
